6e67af7e5d0040df7c1897ed7ae70448b8a9e560
Rosemary Ku <rosemary.ku@hotmail.com>
2022-04-15 16:09:27 -0400

    add about component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ProfilePicture from "../assets/profile-pic.png";
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen bg-[#F7E9D7] text-black">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-full w-full px-4 grid gap-8">
+          <div className="pb-8 pl-4 text-center">
+            <p className="text-4xl font-bold inline border-b-4 border-[#827397]">
+              ABOUT ME
+            </p>
+            <p className="w-30 px-20 mx-20 pt-10 text-justify">
+              Hi, my name is Rosemary and I'm a full-stack developer based in
+              Toronto. I love taking on intricate new challenges and building
+              creative solutions with sound logic and clean code. After seeing
+              first-hand how software could addess many gaps in the healthcare
+              industry, I decided to continue growing my technical skills to
+              build interactive web experiences using Javascript and React.
+            </p>
+            {/* <img
+                className="object-scale-down h-50 w-50 rounded-full pt-10"
+                src={ProfilePicture}
+                alt="Rosemary Ku"
+              /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
