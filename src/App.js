3579117821222f8942e95dd5228f9dd852e7fd0d
Rosemary Ku <rosemary.ku@hotmail.com>
2022-04-15 12:11:38 -0400

    add navbar logic and initial styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <div>
+      <NavBar />
       <p className="text-3xl font-bold underline">Rosemary's Website</p>
     </div>
   );
