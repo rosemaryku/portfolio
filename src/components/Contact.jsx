3191bed013cccafdb97b70c97fa2e22b74267c4a
Rosemary Ku <rosemary.ku@hotmail.com>
2022-04-17 20:44:13 -0400

    add contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#F7E9D7] flex justify-center items-center p-4 text-center"
+    >
+      <form
+        method="POST"
+        action="https://getform.io/f/3b4422f0-3de7-4bec-96fd-599adb4a4df4"
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-[#827397]">
+            CONTACT
+          </p>
+        </div>
+        <input
+          className="bg-[#ffffff] border-[#ffffff] p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="bg-[#ffffff] my-4 p-2"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textArea
+          className="bg-[#ffffff] p-2"
+          placeholder="Message"
+          name="message"
+        />
+        <button className="text-white px-14 py-3 my-6 mx-auto flex items-center bg-[#827397]  border-[#827397] hover:bg-[#3A3845] hover:border-[#3A3845]">
+          SEND
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
